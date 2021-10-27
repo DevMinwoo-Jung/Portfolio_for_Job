b94867720dbbd3c1ecd4434c7fba0007d0c02005
Minwoo <applejung312@gmail.com>
2021-10-27 22:16:53 +0900

    sad

--- minwoo_portfolio/src/components/about/about.jsx
+++ minwoo_portfolio/src/components/about/about.jsx
@@ -29,16 +29,25 @@ const About = () => {
     <div className={style.about__div}>
       <div className={style.introduce__div}>
         <div className={style.para__div}>
-          <p className={style.introduce__para}>Why do we use it?
-It is a long established fact that a reader will
-be distracted by the readable content of a page when looking at its layout. 
-The point of using Lorem Ipsum is that it has a more-or-less normal distribution 
-of letters, as opposed to using 'Content here, content here', making it look like
-readable English. Many desktop publishing packages and web page editors now use
-Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will 
-uncover many web sites still in their infancy. Various versions have evolved over 
-the years, sometimes by accident, sometimes on purpose (injected humour and the
-    like).</p>
+          <p className={style.introduce__header}>About me</p>
+          <p className={style.introduce__h1}>느리더라도 꾸준하게 발전하는 개발자가 되겠습니다.</p>
+          <span className={style.introduce__para}>
+            호서대학교 국제업무팀에서 2년간 재직하면서 교환/어학연수 학생들 관리, 
+            어학 프로그램 관리 등 꼼꼼하게 관리 하는 것에 보람을 느꼈었지만 
+            개인으로서 성장 한다기 보다는 일에만 적응하는 모습을 보며 회의감이 들었습니다.<br/><br/>
+            
+            그러던 중 개발자라는 직업을 알게 되었고, 계속해서 성장할 수 있다는 점에 이끌려 진로를
+            변경하게 되었습니다. 쌍용 교육센터에서 수료 후 솔류션 회사에 입사하였지만, 회사가 원하는 방향과
+            제가 지향하는 것이 달라 아쉽게 퇴사 후 다시 준비중에 있습니다. <br/><br/>
+
+            저는 최신기술에 관심 있지만 기본이 되는 것에도 소흘하지 않고자 하고 있습니다.
+            이를 위해 단순히 남의 코딩을 배끼는 것이 아니라 Youtube, Blog 뿐만 아니라 Mdn과 같은 사이트를 참고하며
+            공부하고 있습니다.  <br/><br/>
+
+            아직은 과도한 성과를 바라기 보다는 기본을 중시하는 신입 개발자가 되고자 합니다.
+            다른 분들과 즐겁게 협업하며 차근차근히 성장하고 싶습니다.
+            이런 저의 성장가능성을 긍정적으로 보고 저를 뽑아줄 회사를 찾고 있습니다.
+          </span>
         </div>
         <div className={style.photo__div}>
 
@@ -47,7 +56,7 @@ the years, sometimes by accident, sometimes on purpose (injected humour and the
       <div className={style.tech__div}>
         <div className={style.showBtn__div}>
             <p className={style.showBtn} onClick={showDivOne}>Main Skills</p>
-            <p className={style.showBtn} onClick={showDivTwo}>Main & Sub Sckills</p>
+            <p className={style.showBtn} onClick={showDivTwo}>Main & Experienced Sckills</p>
         </div>
         {
           divOne === true &&
@@ -120,7 +129,7 @@ the years, sometimes by accident, sometimes on purpose (injected humour and the
         {
           divTwo === true &&
           <div className={style.skills__div}>
-            <p className={style.tech__para}>Main & Sub Sckills</p>
+            <p className={style.tech__para}>Main & Experienced Skills</p>
             <div className={style.skills}>
               <p className={style.skill__para}>Front-end</p>
                 <div className={style.skillDesc__div}>
--- minwoo_portfolio/src/components/project/project.jsx
+++ minwoo_portfolio/src/components/project/project.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import style from './project.module.css';
 import Slider from "react-slick";
+import {BiShowAlt} from 'react-icons/bi';
+import {BsGithub} from 'react-icons/bs';
 
 
 const Project = () => {
@@ -17,14 +19,59 @@ const Project = () => {
   const [hideIntro, setHideIntro] = useState(false);
 
   const showProjectSpec = () => {
-    setShow(true);
-    setHideIntro(true);
+      setShow(true);
+      setHideIntro(true);
+
+  }
+  const hideProjectSpec = () => {
+      setShow(false);
+      setHideIntro(false);
+
   }
 
-  const hideProjectSpec = () => {
-    setShow(false);
-    setHideIntro(false);
+  const [showTwo, setShowTwo] = useState(false);
+  const [hideIntroTwo, setHideIntroTwo] = useState(false);
+
+  const showProjectSpecTwo = () => {
+    setShowTwo(true);
+    setHideIntroTwo(true);
+
   }
+  const hideProjectSpecTwo = () => {
+    setShowTwo(false);
+    setHideIntroTwo(false);
+
+  }
+
+  const [showThree, setShowThree] = useState(false);
+  const [hideIntroThree, setHideIntroThree] = useState(false);
+
+  const showProjectSpecThree = () => {
+    setShowThree(true);
+    setHideIntroThree(true);
+
+  }
+  const hideProjectSpecThree = () => {
+    setShowThree(false);
+    setHideIntroThree(false);
+
+  }
+
+  const [showFour, setShowFour] = useState(false);
+  const [hideIntroFour, setHideIntroFour] = useState(false);
+
+
+  const showProjectSpecFour = () => {
+    setShowFour(true);
+    setHideIntroFour(true);
+
+  }
+  const hideProjectSpecFour = () => {
+    setShowFour(false);
+    setHideIntroFour(false);
+
+  }
+
 
 
 
@@ -32,61 +79,108 @@ const Project = () => {
       <div className={style.content__div}>
         <div className={style.img__div}>
         <Slider {...settings}>
-          <div className={style.project} onMouseEnter={showProjectSpec} onMouseLeave={hideProjectSpec}>
-            <div className={style.img__contatiner}>
-            <div className={`${hideIntro ?  style.project__introhide : style.project__intro }`}>
-              <img className={style.img} src={`/imgs/thumbnail4.png`} alt=""/>
-              <div className={style.img__para}>
-                <span>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
+          <div className={style.project} >
+            <div className={style.img__contatiner} onMouseEnter={showProjectSpec} onMouseLeave={hideProjectSpec}>
+              <div className={`${hideIntro ?  style.project__introhide : style.project__intro }`}>
+                <img className={style.img} src={`/imgs/thumbnail4.png`} alt=""/>
+                <div className={style.img__para}>
+                  <p className={style.spec__head}>MiniGame</p>
+                  <p className={style.spec__para}>일정하게 움직이는 물고기를 시간내에 클릭하는 게임입니다.</p>
+                </div>
+              </div>
+              <div className={`${show ? style.projectSpec__divShow: style.projectSpec__div}`}>
+                <div className={style.specDesc__div}>
+                  <p className={style.spec__head}>#세부사항</p>
+                  <p className={style.spec__para}>참여율 100%</p>
+                  <p className={style.spec__para}>제작기간 10일</p>
+                  <p className={style.spec__head}>#Detail</p>
+                  <p className={style.spec__para}>Vanila Javascript를 이용한 Mini Game으로 Javasciprt의 class, this가 그리키는 것이 무엇인지 등의 문법 이해와 DOM요소의 조작 방식을 이해하는데 도움이 되었습니다.</p>
+                  <p className={style.spec__head}>#기술 스택</p>
+                  <p className={style.spec__para}>Vanila Javascript</p>
+                </div>
               </div>
             </div>
-            <div className={`${show ? style.projectSpec__divShow: style.projectSpec__div}`}>
-              <p>안녕???</p>
-            </div>
-            </div>
             <div className={style.logo__div}>
-                <button>ㅇㅁㄴㅇ</button>
+              <BsGithub className={style.logo}/><p className={style.tooltip__show}> Github</p>     
+              <BiShowAlt className={style.logo}/><p className={style.tooltip__show}> Show Live</p>
             </div>
           </div>
           <div className={style.project}>
-            <div className={style.img__contatiner}>
-            <img className={style.img} src={`/imgs/thumbnail1.png`} alt=""/>
-            <div className={style.img__para}>
-              <span>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </span>
-            </div>
+            <div className={style.img__contatiner} onMouseEnter={showProjectSpecTwo} onMouseLeave={hideProjectSpecTwo}>
+              <div className={`${hideIntroTwo ?  style.project__introhide : style.project__intro }`}>
+              <img className={style.img} src={`/imgs/thumbnail4.png`} alt=""/>
+                <div className={style.img__para}>
+                  <p className={style.spec__head}>Financial-ledger</p>
+                  <p className={style.spec__para}>가계부를 제작하였습니다.</p>
+                </div>
+              </div>
+              <div className={`${showTwo ? style.projectSpec__divShow: style.projectSpec__div}`}>
+                <div className={style.specDesc__div}>
+                  <p className={style.spec__head}>#세부사항</p>
+                  <p className={style.spec__para}>참여율 100%</p>
+                  <p className={style.spec__para}>제작기간 3주</p>
+                  <p className={style.spec__head}>#Detail</p>
+                  <p className={style.spec__para}>React hooks를 이용하여 가계부를 제작하여 props, usestate등 리액트 전반을 이해할 수 있는데 도움이 되었으며 cloudinary, firebase API를 이용하여 이미지 업로드, db사용을 하였습니다. 또한 react-router를 이용하였습니다.</p>
+                  <p className={style.spec__head}>#기술 스택</p>
+                  <p className={style.spec__para}>Javascript, React, React-router, cloudinary, firebase</p>
+                </div>
+              </div>
             </div>
             <div className={style.logo__div}>
-                <button>ㅇㅁㄴㅇ</button>
+              <BsGithub className={style.logo}  /><p className={style.tooltip__show}> Github</p>     
+              <BiShowAlt className={style.logo} /><p className={style.tooltip__show}> Show Live</p>
             </div>
           </div>
           <div className={style.project}>
-            <div className={style.img__contatiner}>
-            <img className={style.img} src={`/imgs/thumbnail2.png`} alt=""/>
-            <div className={style.img__para}>
-              <span>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </span>
-            </div>
+            <div className={style.img__contatiner} onMouseEnter={showProjectSpecThree} onMouseLeave={hideProjectSpecThree}>
+              <div className={`${hideIntroThree ?  style.project__introhide : style.project__intro }`}>
+                <img className={style.img} src={`/imgs/download.png`} alt=""/>
+                <div className={style.img__para}>
+                    <p className={style.spec__head}>Portfolio</p>
+                    <p className={style.spec__para}>지금 보고 계시는 포트폴리오를 제작하였습니다.</p>
+                </div>
+              </div>
+              <div className={`${showThree ? style.projectSpec__divShow: style.projectSpec__div}`}>
+                <div className={style.specDesc__div}>
+                    <p className={style.spec__head}>#세부사항</p>
+                    <p className={style.spec__para}>참여율 100%</p>
+                    <p className={style.spec__para}>제작기간 5일</p>
+                    <p className={style.spec__head}>#Detail</p>
+                    <p className={style.spec__para}>React, React-router 주로 이용하였고 빠른 개발을 위해 react-slick library를 이용하였습니다.</p>
+                    <p className={style.spec__head}>#기술 스택</p>
+                    <p className={style.spec__para}>React, Javascript</p>
+                </div>
+              </div>
             </div>
             <div className={style.logo__div}>
-                <button>ㅇㅁㄴㅇ</button>
+              <BsGithub className={style.logo}  /><p className={style.tooltip__show}> Github</p>     
+              <BiShowAlt className={style.logo} /><p className={style.tooltip__show}> Show Live</p>
             </div>
           </div>
-          <div className={style.project}>
+          <div className={style.project} onMouseEnter={showProjectSpecFour} onMouseLeave={hideProjectSpecFour}>
             <div className={style.img__contatiner}>
-            <img className={style.img} src={`/imgs/thumbnail3.png`} alt=""/>
-            <div className={style.img__para}>
-              <span>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </span>
-            </div>
+              <div className={`${hideIntroFour ?  style.project__introhide : style.project__intro }`}>
+                <img className={style.img} src={`/imgs/reset.png`} alt=""/>
+                <div className={style.img__para}>
+                      <p className={style.spec__head}>MiniGame</p>
+                      <p className={style.spec__para}>일정하게 움직이는 물고기를 시간내에 클릭하는 게임입니다.</p>
+                </div>
+              </div>
+            <div className={`${showFour ? style.projectSpec__divShow: style.projectSpec__div}`}>
+              <div className={style.specDesc__div}>
+                      <p className={style.spec__head}>#세부사항</p>
+                      <p className={style.spec__para}>참여율 100%</p>
+                      <p className={style.spec__para}>제작기간 10일</p>
+                      <p className={style.spec__head}>#Detail</p>
+                      <p className={style.spec__para}>Vanila Javascript를 이용한 Mini Game으로 Javasciprt의 class, this가 그리키는 것이 무엇인지 등의 문법 이해와 DOM요소의 조작 방식을 이해하는데 도움이 되었습니다.</p>
+                      <p className={style.spec__head}>#기술 스택</p>
+                      <p className={style.spec__para}>Vanila Javascript</p>
+              </div>
+              </div>
             </div>
             <div className={style.logo__div}>
-                <button>ㅇㅁㄴㅇ</button>
+              <BsGithub className={style.logo}  /><p className={style.tooltip__show}> Github</p>     
+              <BiShowAlt className={style.logo} /><p className={style.tooltip__show}> Show Live</p>
             </div>
           </div>
         </Slider>
