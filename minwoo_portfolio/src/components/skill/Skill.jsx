ec574e89d83c4e14c7bd24a504311f2caeeef7cc
Minwoo <applejung312@gmail.com>
2022-01-30 10:01:58 +0900

    20220130

--- minwoo_portfolio/src/components/skill/Skill.jsx
+++ minwoo_portfolio/src/components/skill/Skill.jsx
@@ -18,24 +18,23 @@ const Skill = ({ data }) => {
         onMouseEnter={showProgressBar}
         onMouseLeave={hideProgressBar}
         key={Math.random()}
-        className="hover:opacity-10 hover:duration:700 transition ease-in-out m-auto mt-4 mb-4 divide-solid 2xl:h-44 lg:h-28 2xl:w-44 lg:w-28 p-4 max-w-xs"
+        className="relative hover:opacity-10 hover:duration:700 transition ease-in-out m-auto mt-4 mb-4 divide-solid 2xl:h-44 lg:h-28 2xl:w-44 lg:w-28 p-4 max-w-xs"
       >
+        {progressBar && (
+          <ProgressBar
+            className="absolute hover:opacity-100 m-auto mt-4 mb-4 divide-solid 2xl:h-44 lg:h-28 2xl:w-44 lg:w-28 p-4 max-w-xs"
+            variant="info"
+            now={data.progressBar}
+            label={`${data.progressBar}%`}
+          />
+        )}
         <img
           src={data.imgSrc}
           alt=""
           className="rounded-lg 2xl:h-32 2xl:w-32 lg:h-14 lg:w-14"
         />
-        <p className="text-center">{data.skillName}</p>
+        <p className="hover:opacity-100 text-center">{data.skillName}</p>
       </div>
-      {progressBar && (
-        <ProgressBar
-          className="hover:opacity-100 m-auto mt-4 mb-4 divide-solid 2xl:h-44 lg:h-28 2xl:w-44 lg:w-28 p-4 max-w-xs"
-          animated
-          variant="info"
-          now={data.progressBar}
-          label={`${data.progressBar}%`}
-        />
-      )}
     </>
   );
 };
--- minwoo_portfolio/src/components/skills/Skills.jsx
+++ minwoo_portfolio/src/components/skills/Skills.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { skills } from "../data.js";
-import Skill from '../skill/Skill.jsx';
+import Skill from "../skill/Skill.jsx";
 import "./skills.scss";
 
 const Skills = () => {
-
-
-
   return (
     <div className="justify-center grid 2xl:grid-cols-6 lg:grid-cols-4 2xl:ml-36 lg:ml-10 2xl:mr-36 lg:mr-10">
       {Object.keys(skills)
         .map((key) => skills[key])
         .map((data) => (
-          <Skill data={data} key={Math.random}/>
+          <Skill data={data} key={Math.random} />
         ))}
     </div>
   );
